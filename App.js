c3127f1fead8a3363e7ab5cd751f53990a8c30f1
denervalspelizario <denervalspelizario@gmail.com>
2023-02-20 15:27:16 -0300

    update appBuscadorCep criando input que recebe dado e joga na state cep

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'; 
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
 import api from './src/service/api'; // importando api com a base da url
 
 export default function App() {
+
+  const [cep, setCep] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Hello Buscador</Text>
-    </View>
+    <SafeAreaView style={styles.container}> 
+      <View style={styles.containerView}>
+        <Text>Digite o cep desejado</Text>
+        <TextInput
+          style={styles.input}  
+          placeholder='Ex: 08750500'
+          value={cep} // 1 seu valor será state cep
+          onChangeText={(texto) => setCep(texto)} // 1 ao digitar os dados irão a state cep atravez da setCep
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: '10%'
   },
+  containerView:{
+    alignItems: 'center',
+  }
 });
