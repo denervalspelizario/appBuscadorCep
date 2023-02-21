53f179847462ba60d1cd7938fcef8d0059e2fdaf
denervalspelizario <denervalspelizario@gmail.com>
2023-02-21 06:34:45 -0300

    update appBuscadorCep estilizacao de input container

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React, {useState} from 'react'; 
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
-import api from './src/service/api'; // importando api com a base da url
+import api from './src/service/api'; // 1 importando api com a base da url
 
 export default function App() {
 
-  const [cep, setCep] = useState('')
+  const [cep, setCep] = useState('');
 
   return (
     <SafeAreaView style={styles.container}> 
       <View style={styles.containerView}>
-        <Text>Digite o cep desejado</Text>
+        <Text style={styles.textCep}>Digite o cep desejado</Text>
         <TextInput
           style={styles.input}  
           placeholder='Ex: 08750500'
           value={cep} // 1 seu valor será state cep
           onChangeText={(texto) => setCep(texto)} // 1 ao digitar os dados irão a state cep atravez da setCep
+          keyboardType='numeric' // 1 apenas teclado numerico
+          maxLength={8} // 1 limitando a apenas 8 digitos
+          autoComplete='postal-address' // auto complete para cep
         />
       </View>
     </SafeAreaView>
@@ -24,9 +27,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: '10%'
+    paddingTop: '10%',
+    backgroundColor: '#E0FFFF'
   },
   containerView:{
     alignItems: 'center',
+  },
+  textCep:{
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: '600',
+    color: '#00CED1'
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    width: '90%',
+    padding: 10,
+    fontSize: 15
   }
 });
